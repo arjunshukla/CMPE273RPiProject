8c03361a5fa9e2b8e18bcb71f542e9d1ad6cae11
praveenkarkhile <praveenkarkhile@gmail.com>
2015-05-10 18:17:32 -0700

    Modified the class details

--- Illuminati/src/main/java/com/cmpe273/illuminati/services/IdentifyNewFiles.java
+++ Illuminati/src/main/java/com/cmpe273/illuminati/services/IdentifyNewFiles.java
@@ -1,13 +1,36 @@
 package com.cmpe273.illuminati.services;
 
+
+import java.util.List;
+
 /**
  * Created by praveenkarkhile on 5/9/15.
  */
+
+    import java.nio.file.Files;
+
+
 public class IdentifyNewFiles {
 
-    public void nefiles(){
+    Files file;
+
+    /**
+     * Checks for the new file added in Local drive
+     * @param  - Path of the Local drive
+     * @param  - find the new files added
+     * @return array list of new files
+     */
+
+    public void newfiles(){
 
     }
 
+    public List<String> filesInLocal(){
+
+        return null;
+
+    }
 
 }
+
+
