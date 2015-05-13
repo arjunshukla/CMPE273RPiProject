719fa9b15d8ab4b5a964ebcb65e8a8f3174363f1
Arjun Shukla <arjun.shukla@outlook.com>
2015-05-12 18:27:44 -0700

    Adding meta data comparison class files

---
+++ Illuminati/src/main/java/com/cmpe273/illuminati/services/CompareMetaDataService.java
@@ -0,0 +1,19 @@
+package com.cmpe273.illuminati.services;
+
+/**
+ * Created by arjunshukla on 5/9/15.
+ */
+
+import java.util.ArrayList;
+
+public interface CompareMetaDataService {
+
+public ArrayList<String>[] returnFileListToUploadAndDownload();
+
+    ArrayList<String> arrListServer = new ArrayList<String>();
+    ArrayList<String> arrListLocal = new ArrayList<String>();
+
+//    name(File var)
+
+
+}
---
+++ Illuminati/src/main/java/com/cmpe273/illuminati/services/CompareMetaDataServiceImpl.java
@@ -0,0 +1,22 @@
+package com.cmpe273.illuminati.services;
+
+import java.util.ArrayList;
+
+/**
+ * Created by arjunshukla on 5/9/15.
+ */
+
+public class CompareMetaDataServiceImpl implements CompareMetaDataService {
+
+    @Override
+    public ArrayList<String>[] returnFileListToUploadAndDownload() {
+        /*  Method implementation:
+            1. Call Praveen’s method to get deltas as arrayList for upload and download
+            2. Identify the modified files from given delta ArratLists
+            3. return the result as an array of ArrayLists to Sneha for upload and download operations
+        */
+
+
+        return null;//new ArrayList<String>();
+    }
+}
