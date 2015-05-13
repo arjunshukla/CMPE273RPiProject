c3ac699e5ec106927d4230ac3bbe0c4b73c13e72
Arjun Shukla <arjun.shukla@outlook.com>
2015-05-12 20:32:12 -0700

    Changed dropbox path to "Volumes"

--- Illuminati/src/main/java/com/cmpe273/illuminati/services/CompareMetaDataServiceImpl.java
+++ Illuminati/src/main/java/com/cmpe273/illuminati/services/CompareMetaDataServiceImpl.java
@@ -1,25 +1,40 @@
 package com.cmpe273.illuminati.services;
 
+import com.dropbox.core.DbxEntry;
+import com.dropbox.core.DbxException;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by arjunshukla on 5/9/15.
  */
-
+@Service
 public class CompareMetaDataServiceImpl implements CompareMetaDataService {
 
     @Override
-    public ArrayList<String>[] returnFileListToUploadAndDownload() {
+    public ArrayList<String>[] returnFileListToUploadAndDownload() throws IOException, DbxException {
         /*  Method implementation:
             1. Call Praveen’s method to get deltas as arrayList for upload and download
             2. Identify the modified files from given delta ArratLists
             3. return the result as an array of ArrayLists to Sneha for upload and download operations
         */
-
-        ArrayList<String> arrListServer = new ArrayList<String>();
-        ArrayList<String> arrListLocal = new ArrayList<String>();
-
-
+        try {
+//        List<File> arrListLocal = new List<String>();
+        GetFolderInfoForDrpBoxServiceImpl objGetFolderInfo = new GetFolderInfoForDrpBoxServiceImpl();
+        DbxEntry.WithChildren arrFileListFromServer;
+        arrFileListFromServer = objGetFolderInfo.getFilelistFromDrpBox("/Volumes");
+        for(DbxEntry child: arrFileListFromServer.children){
+                System.out.println(child);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (DbxException e) {
+            e.printStackTrace();
+        }
         return null;//new ArrayList<String>();
     }
 }
