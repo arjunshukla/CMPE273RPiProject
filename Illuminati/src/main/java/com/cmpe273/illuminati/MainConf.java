b90595a1bbf977e26a94b9b97201e4253833262f
Ashutosh Chandane <ashutoshchandane7390@gmail.com>
2015-05-13 17:33:37 -0700

    Schedular changes

--- Illuminati/src/main/java/com/cmpe273/illuminati/MainConf.java
+++ Illuminati/src/main/java/com/cmpe273/illuminati/MainConf.java
@@ -9,7 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableAutoConfiguration
-@EnableScheduling
 @ComponentScan
 public class MainConf {
 
--- Illuminati/src/main/java/com/cmpe273/illuminati/SchedularConf.java
+++ Illuminati/src/main/java/com/cmpe273/illuminati/SchedularConf.java
@@ -6,6 +6,7 @@ import com.dropbox.core.DbxException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
 
@@ -13,11 +14,13 @@ import java.io.IOException;
  * Created by ASHU on 13-05-2015.
  */
 @Component
+@RequestMapping("/Schedular")
 public class SchedularConf {
     @Autowired
     CompareMetaDataService compareMetaDataService;
 
     @Scheduled(fixedRate = 30000)
+    @RequestMapping("/Start")
     public void uploadScheduler() throws IOException, DbxException {
         System.out.println("Scheduler Started");
         compareMetaDataService.returnFileListToUploadAndDownload();
